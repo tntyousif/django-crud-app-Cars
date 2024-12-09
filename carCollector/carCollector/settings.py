6d7328cdd4cb47bd2c78d0efa8458d01421b4df0
tntyousif <yousif02bh@gmail.com>
2024-12-09 13:13:40 +0300

    adding the auth and fixing issues

--- carCollector/carCollector/settings.py
+++ carCollector/carCollector/settings.py
@@ -122,3 +122,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Auth
+# Add this variable to specify where decorators and mixins should redirect to
+LOGIN_URL = 'home'
+
+# Add this variable to specify where successful logins should redirect to
+LOGIN_REDIRECT_URL = 'car-index'
+
+# Add this variable to specify where logging out redirects to
+LOGOUT_REDIRECT_URL = 'home'
