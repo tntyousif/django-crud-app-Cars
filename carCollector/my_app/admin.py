5e78ceec7b0f5467645508d63c4c9d970d77a35b
tntyousif <yousif02bh@gmail.com>
2024-12-09 13:18:00 +0300

    adding the Modification to the admin

--- carCollector/my_app/admin.py
+++ carCollector/my_app/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Car, Filling
+from .models import Car, Filling, Modification
 
 # Register your models here.
 
 admin.site.register(Car)
 
 admin.site.register(Filling)
+
+admin.site.register(Modification)
